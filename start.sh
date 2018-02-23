6a8676a4ff36a9d3db6d37c156e247df548c1143
Ben Fairfield <benfa@umich.edu>
2018-02-23 09:07:29 -0500

    Commented parts of start.sh which will be in local.start.sh

--- start.sh
+++ start.sh
@@ -9,10 +9,11 @@ ln -sf /dev/stderr /var/log/apache2/error_log
 # Apache config files if /etc/apache2 is ServerRoot
 ln -sf /scd-secret/apache2.conf /etc/apache2/apache2.conf
 ln -sf /scd-secret/ports.conf /etc/apache2/ports.conf
-ln -sf /scd-secret/settings.php /var/www/html/sites/default/settings.php
-ln -s /var/www/html/ /var/www/html/dataguide
-mkdir /var/www/html/sites/safecomputing-dev.openshift.dsc.umich.edu.dataguide
-ln -s /scd-secret/dataguide-settings.php \
+ln -sf /scd-secret/app/settings.php /var/www/html/sites/default/settings.php
+
+#ln -s /var/www/html/ /var/www/html/dataguide
+#mkdir /var/www/html/sites/safecomputing-dev.openshift.dsc.umich.edu.dataguide
+#ln -s /scd-secret/dataguide-settings.php \
 	/var/www/html/sites/safecomputing-dev.openshift.dsc.umich.edu.dataguide/settings.php
 
 ## SSL and Cosign
@@ -21,8 +22,8 @@ ln -sf /scd-secret/cosign.conf /etc/apache2/mods-available/cosign.conf
 ln -sf /scd-secret/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /scd-secret/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /scd-secret/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
-ln -sf /scd-secret/safecomputing-dev.openshift.dsc.umich.edu.cert /etc/ssl/certs/safecomputing-dev.openshift.dsc.umich.edu.cert
-ln -sf /scd-secret/safecomputing-dev.openshift.dsc.umich.edu.key /etc/ssl/private/safecomputing-dev.openshift.dsc.umich.edu.key
+#ln -sf /scd-secret/safecomputing-dev.openshift.dsc.umich.edu.cert /etc/ssl/certs/safecomputing-dev.openshift.dsc.umich.edu.cert
+#ln -sf /scd-secret/safecomputing-dev.openshift.dsc.umich.edu.key /etc/ssl/private/safecomputing-dev.openshift.dsc.umich.edu.key
 # Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs
 
