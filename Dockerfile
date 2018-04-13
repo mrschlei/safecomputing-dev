08da6449e17ca6b374963e434de3c7b56abe5606
Ben Fairfield <benfa@umich.edu>
2018-04-13 10:29:12 -0400

    New Dockerfile and start.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM drupal:7.56-apache
+FROM drupal:7.58-apache
 
-#MAINTAINER: Ben Fairfield - benfa
+#MAINTAINER: 
 
 #### Cosign Pre-requisites ###
 WORKDIR /usr/lib/apache2/modules
@@ -13,7 +13,7 @@ ENV APACHE2=/usr/sbin/apache2
 # install PHP and Apache2 here
 RUN apt-get update \
 	&& apt-get install -y wget gcc make openssl \
-		libssl-dev=$OPENSSL_VERSION apache2-dev autoconf
+		libssl-dev apache2-dev autoconf net-tools
 
 ### Build Cosign ###
 RUN wget "$COSIGN_URL" \
@@ -30,15 +30,32 @@ RUN wget "$COSIGN_URL" \
 	&& mkdir -p /var/cosign/filter \
 	&& chmod 777 /var/cosign/filter
 
-#WORKDIR /etc/apache2
-
 ### Remove pre-reqs ###
 RUN apt-get remove -y make wget autoconf \
 	&& apt-get autoremove -y
 
-EXPOSE 443
+# Section that sets up Apache and Cosign to run as non-root user.
+EXPOSE 8080
+EXPOSE 8443
 
+# nothing here for the time being.
 COPY . /var/www/html/
+
+### There may be an easier way to do all of this by setting APACHE_RUN_USER
+### and APACHE_RUN_GROUP in env vars or /etc/apache2/envvars
+
+### change directory owner, as openshift user is in root group.
+RUN chown -R root:root /var/www/html /var/log/apache2 /var/lock/apache2 \
+	/var/run/apache2 /run/lock
+
+### Modify perms for the openshift user, who is not root, but part of root group.
+RUN chmod -R g+r /var/www/html /var/cosign 
+RUN chmod -R g+rw /var/log/apache2 /var/www/html/sites/default /etc/apache2 \
+	/etc/ssl/certs /etc/ssl/private /etc/apache2/mods-enabled /etc/apache2/sites-enabled \
+	/etc/apache2/sites-available /etc/apache2/mods-available \
+	/var/lib/apache2/module/enabled_by_admin /var/lib/apache2/site/enabled_by_admin \
+	/var/lock/apache2 /var/run/apache2
+RUN chmod g+rwx /etc/ssl/private
 
 ### Start script incorporates config files and sends logs to stdout ###
 COPY start.sh /usr/local/bin
--- start.sh
+++ start.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
 
-# for use with drupal:8.4.2 image
-
 # Redirect logs to stdout and stderr for docker reasons.
-ln -sf /dev/stdout /var/log/apache2/access_log
-ln -sf /dev/stderr /var/log/apache2/error_log
+# it seems both sylinks already exist. 
+# these commands create unnecessary duplicates
+#ln -sf /dev/stdout /var/log/apache2/access_log
+#ln -sf /dev/stderr /var/log/apache2/error_log
 
-# Apache config files if /etc/apache2 is ServerRoot
+# apache and virtual host secrets
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
-ln -sf /secrets/apache2/ports.conf /etc/apache2/ports.conf
+ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
+
+# SSL secrets
+ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
+ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
+ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
 
 if [ -f /secrets/app/local.start.sh ]
 then
   /bin/sh /secrets/app/local.start.sh
 fi
 
-## SSL and Cosign
-ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
-ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
-ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
-ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
-ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
-
-# Rehash command needs to be run before starting apache.
-c_rehash /etc/ssl/certs
+## Rehash command needs to be run before starting apache.
+c_rehash /etc/ssl/certs >/dev/null
 
 a2enmod ssl
 a2enmod include
 a2ensite default-ssl 
 
-# set SGID for www-data 
-chown -R www-data.www-data /var/www/html /var/cosign
-chmod -R 2775 /var/www/html /var/cosign
+#cd /var/www/html
+#drush @sites cc all --yes
+#drush up --no-backup --yes
 
 /usr/local/bin/apache2-foreground
